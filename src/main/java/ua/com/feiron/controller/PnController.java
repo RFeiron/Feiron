c6fc21fdb4a1c244026220c8520269353a709fd0
Mirzoiev Ruslan <feiron@mail.ua>
2016-04-24 22:36:13 +0300

    Развертывается как на локальном репозитории, так и на хостинге openshift(доработать базу данных и авторизацию)

--- src/main/java/ua/com/feiron/controller/PnController.java
+++ src/main/java/ua/com/feiron/controller/PnController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ua.com.feiron.domain.Pn;
@@ -21,7 +20,7 @@ public class PnController {
     private PnValidator pnValidator;
 
     @Autowired
-    public PnController(PnRepository pnRepository, PnValidator pnValidator){
+    public PnController(PnRepository pnRepository, PnValidator pnValidator) {
         this.pnRepository = pnRepository;
         this.pnValidator = pnValidator;
 
@@ -29,28 +28,30 @@ public class PnController {
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String getPr(){
+    public String getPr() {
         return "index";
     }
 
     @RequestMapping(value = "product", method = RequestMethod.GET)
-    public String getProduct(Model model){
+    @PreAuthorize("isAuthenticated()")
+    public String getProduct(Model model) {
         List<Pn> lists = this.pnRepository.listAll();
         model.addAttribute("lists", lists);
         return "product";
     }
+
     @RequestMapping(value = "/addProduct", method = RequestMethod.GET)
     @PreAuthorize("isAuthenticated()")
-    public String addProduct(Model model){
+    public String addProduct(Model model) {
         model.addAttribute("pn", new Pn());
         return "addProduct";
     }
 
     @RequestMapping(value = "/addProduct", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated()")
-    public String addProduct(@ModelAttribute("pn") Pn pn, BindingResult bindingResult){
+    public String addProduct(@ModelAttribute("pn") Pn pn, BindingResult bindingResult) {
         this.pnValidator.validate(pn, bindingResult);
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return "addProduct";
         }
 
@@ -60,7 +61,7 @@ public class PnController {
 
     @RequestMapping(value = "deleteProduct/{id}", method = RequestMethod.GET)
     @PreAuthorize("hasRole('admin')")
-    public String deleteProduct(@PathVariable Integer id){
+    public String deleteProduct(@PathVariable Integer id) {
 
         this.pnRepository.removeProduct(id);
         return "redirect:/product";
@@ -72,43 +73,62 @@ public class PnController {
 //    }
 
     @RequestMapping(value = "calc", method = RequestMethod.GET)
-    public ModelMap say(@ModelAttribute("pn")Pn pn, ModelMap modelMap){
-        modelMap = new ModelMap();
-        if (pn.getA() != null && pn.getB() != null && pn.getWork_b() != null && pn.getBase_b() != null ){
-            modelMap.addAttribute("a", pn.getA());
-            modelMap.addAttribute("b", pn.getB());
-            modelMap.addAttribute("work_B", pn.getWork_b());
-            modelMap.addAttribute("bas_B", pn.getBase_b());
-            modelMap.addAttribute("result", ((Math.ceil(pn.getB() / pn.getWork_b())) * pn.getA() * pn.getBase_b()));
-            modelMap.addAttribute("sam", (int)((pn.getB() / pn.getWork_b()) * pn.getA() * pn.getBase_b() * 6));
-            modelMap.addAttribute("kl",(int)(Math.ceil(pn.getB() / pn.getWork_b())));
+    @PreAuthorize("isAuthenticated()")
+    public Model say(Double work_b, Double base_b, Double a,
+                     Double b, Model model, Pn pn){
+        List<Pn> lists = this.pnRepository.listAll();
 
-        }else {
-            modelMap.addAttribute("result",  "" );
-        }
-        return modelMap;
-    }
-
-    @RequestMapping(value = "calc1", method = RequestMethod.GET)
-    public ModelMap twoSk(@RequestParam(required = false) Double work_B, @RequestParam(required = false) Double bas_B,
-                          @RequestParam(required = false) Double a, @RequestParam(required = false) Double b,
-                          @RequestParam(required = false) Double a2, @RequestParam(required = false) Double b2){
-        ModelMap modelMap = new ModelMap();
-        if (a != null && b != null && work_B != null && bas_B != null ){
-            modelMap.addAttribute("a", a);
-            modelMap.addAttribute("b", b);
-            modelMap.addAttribute("a2", a2);
-            modelMap.addAttribute("b2", b2);
-            modelMap.addAttribute("work_B", work_B);
-            modelMap.addAttribute("bas_B", bas_B);
-            modelMap.addAttribute("result", (((Math.ceil(b / work_B)) * a * bas_B) + ((Math.ceil(b2 / work_B)) * a2 * bas_B)));
-            modelMap.addAttribute("sam", (int)((((Math.ceil(b / work_B)) * a * bas_B) + ((Math.ceil(b2 / work_B)) * a2 * bas_B))) * 6);
-            modelMap.addAttribute("kl",(int)(Math.ceil((b / work_B) + (b2 / work_B))));
+        model.addAttribute("lists", lists);
+        if (a != null && b != null && base_b != null && work_b != null ){
+            model.addAttribute("a", a);
+            model.addAttribute("b", b);
+            model.addAttribute("work_b", pn.getWork_b());
+            model.addAttribute("base_b", pn.getBase_b());
+            model.addAttribute("result", ((Math.ceil(b / work_b) * a * work_b)));
+            model.addAttribute("sam", (int)((b / work_b * a * base_b * 6)));
+            model.addAttribute("kl",(int)(Math.ceil(b / work_b)));
 
 
         }else {
-            modelMap.addAttribute("result",  "" );
+            model.addAttribute("result",  "" );
         }
-        return modelMap;
+        return model;
     }
+    @RequestMapping(value = "triangle", method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    public Model triangle(Double work_b, Double base_b,
+                          Double b, Double c, Double h, Model model){
+        if (b != null && h != null && base_b != null && work_b != null ) {
+            model.addAttribute("b", b);
+            model.addAttribute("h", h);
+            model.addAttribute("work_b", work_b);
+            model.addAttribute("base_b", base_b);
+            model.addAttribute("result", ((Math.ceil(b / work_b) * base_b) * h) / 2);// Кол-во м.кв. материала
+            model.addAttribute("sam", (int) (((Math.ceil(b / work_b) * base_b) * h)/2) * 6); // сол-во крепежа
+            model.addAttribute("kl", (int) ((Math.ceil(b / work_b)))); // кол-во листов
+        }else {
+            model.addAttribute("result", "");
+        }
+        return model;
+    }
+
+    @RequestMapping(value = "trapezium", method = RequestMethod.GET)
+    @PreAuthorize("isAuthenticated()")
+    public Model trapezium(Double work_b, Double base_b,
+                           Double a, Double b, Double h, Model model){
+        if (b != null && h != null && base_b != null && work_b != null ) {
+            model.addAttribute("a", a);
+            model.addAttribute("b", b);
+            model.addAttribute("h", h);
+            model.addAttribute("work_b", work_b);
+            model.addAttribute("base_b", base_b);
+            model.addAttribute("result", ((a + (Math.ceil(b / work_b) * base_b)) / 2) * h);// Кол-во м.кв. материала
+            model.addAttribute("sam", (int) ((a + (Math.ceil(b / work_b) * base_b) / 2) * h) * 6); // сол-во крепежа
+            model.addAttribute("kl", (int) ((Math.ceil(b / work_b) * base_b))); // кол-во листов
+        }else {
+            model.addAttribute("result", "");
+        }
+        return model;
+    }
+
 }
--- src/main/java/ua/com/feiron/repository/SignInRepository.java
+++ src/main/java/ua/com/feiron/repository/SignInRepository.java
@@ -1,7 +1,24 @@
 package ua.com.feiron.repository;
 
-/**
- * Created by 1 on 12.02.2016.
- */
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ua.com.feiron.domain.SignInForm;
+
+import java.util.List;
+
+@Repository
+@Transactional
 public class SignInRepository {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void addsignup(SignInForm signInForm) {
+        this.sessionFactory.getCurrentSession().save(signInForm);
+    }
+    public List<SignInForm> listAll() {
+        return this.sessionFactory.getCurrentSession().createQuery("from SignInForm").list();
+    }
+
 }
